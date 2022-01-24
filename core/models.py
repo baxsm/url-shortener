fd4ff30f9c34f4abac2b35066ace80b9040f33bc
Bobby Aleem <bobbybeatmaker@hotmail.com>
2022-01-24 18:22:13 +0500

    migrations

--- core/models.py
+++ core/models.py
@@ -7,3 +7,5 @@ class ShortUrls(db.Model):
     original_url = db.Column(db.String(500), nullable=False)
     short_id = db.Column(db.String(20), nullable=False, unique=True)
     created_at = db.Column(db.DateTime(), default=datetime.now(), nullable=False)
+
+
